932b62c57810ea8fee4cb3f66c1f854bea5df1ad
Nikita Romantsev <atikinvestnamor@gmail.com>
2024-04-05 22:59:55 +0300

    fixed veggiesTests

--- src/test/java/WaresPageTests.java
+++ src/test/java/WaresPageTests.java
@@ -21,7 +21,21 @@ public class WaresPageTests {
     }
 
     @Test
-    public void addingExoticVeggie(){
+    public void addingAVeggie(){
+        openTheWaresPage();
+        WebElement addBtn = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div[1]/button"));
+        addBtn.click();
+        WebElement nameField = webDriver.findElement(By.id("name"));
+        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        String veg = "капуста";
+        nameField.sendKeys(veg);
+        WebElement saveBtn = webDriver.findElement(By.id("save"));
+        saveBtn.click();
+        WebElement addedVeg = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/table/tbody/tr[5]/td[1]"));
+        Assertions.assertEquals(veg, addedVeg.getText());
+    }
+    @Test
+    public void addingAnExoticVeggie(){
         openTheWaresPage();
         WebElement addBtn = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div[1]/button"));
         addBtn.click();
@@ -29,42 +43,24 @@ public class WaresPageTests {
         webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         String veg = "артишок";
         nameField.sendKeys(veg);
+        WebElement exoticChechBox = webDriver.findElement(By.id("exotic"));
+        exoticChechBox.click();
         WebElement saveBtn = webDriver.findElement(By.id("save"));
         saveBtn.click();
         WebElement addedVeg = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/table/tbody/tr[5]/td[1]"));
         WebElement exoCheckBox = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/table/tbody/tr[5]/td[3]"));
         Assertions.assertAll(
                 () -> Assertions.assertEquals(veg, addedVeg.getText()),
-                () -> Assertions.assertEquals("false", exoCheckBox.getText()));
-    }
-
-    @Test
-    public void addingAVeggie(){
-        openTheWaresPage();
-        webDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        WebElement addBtn = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div[1]/button"));
-        addBtn.click();
-/*        WebElement modalWindowHeader = webDriver.findElement(By.id("editModalLabel"));
-        if(!modalWindowHeader.isDisplayed()){
-            Assertions.fail("Option window wasn't opened");
-        }*/
-        WebElement nameField = webDriver.findElement(By.id("name"));
-        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        String veg = "капуста";
-        nameField.sendKeys(veg);
-/*        WebElement typeField = webDriver.findElement(By.id("type"));
-        System.out.println(typeField.getText());*/
-        WebElement saveBtn = webDriver.findElement(By.id("save"));
-        saveBtn.click();
-        WebElement addedVeg = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/table/tbody/tr[6]/td[1]"));
-        Assertions.assertEquals(veg, addedVeg.getText());
+                () -> Assertions.assertEquals("true", exoCheckBox.getText()));
     }
 
 
-    @AfterAll
+
+
+/*    @AfterAll
     public static void afterAll(){
         webDriver.quit();
-    }
+    }*/
     public static void openThePageAndMaximize(){
         webDriver.get("http://localhost:8080");
         webDriver.manage().window().maximize();
